578c7086b65bdc33e555c56085aca2013851de8f
CarlEmilKoehn <carledk@live.dk>
2025-03-26 12:08:05 +0100

    gameloop update

--- src/Game.java
+++ src/Game.java
@@ -6,30 +6,32 @@ public class Game {
     static TextUI ui = new TextUI();
     static FileIO io = new FileIO();
     private String name;
-    private int maxPlayers ;
+    private int maxPlayers;
     private ArrayList<Player> players;
     private Player currentPlayer;
+    private int minPlayers;
 
-    public Game(String name, int maxPlayers){
+    public Game(String name, int minPlayers, int maxPlayers) {
         this.name = name;
+        this.minPlayers = minPlayers;
         this.maxPlayers = maxPlayers;
         players = new ArrayList<>();
 
     }
 
 
-    public void startSession(){
+    public void startSession() {
         ArrayList<String> data = io.readData("data/playerData.csv");
-        ui.displayMessage("Velkommen til "+ this.name);
+        ui.displayMessage("Velkommen til " + this.name);
 
-        if(!data.isEmpty() && ui.promptBinary("would your like to continue previous game: Y/N")){
-            for(String s : data){
-              String[] values =  s.split(",");//  "tess, 0"
+        if (!data.isEmpty() && ui.promptBinary("Vil du fortsætte fra sidste gemte spil: Y/N")) {
+            for (String s : data) {
+                String[] values = s.split(",");//  "tess, 0"
                 int score = Integer.parseInt(values[1].trim());
-               createPlayer(values[0],score);
+                createPlayer(values[0], score);
             }
 
-        }else{
+        } else {
 
             registerPlayers();
         }
@@ -37,38 +39,40 @@ public class Game {
     }
 
 
-    public void registerPlayers(){
+    public void registerPlayers() {
 
         int totalPlayers = 0;
 
         try {
-            totalPlayers = ui.promptNumeric("Number of players:");       //Konvertere svaret til et tal
+            totalPlayers = ui.promptNumeric("Indtast antal spillere:");       //Konvertere svaret til et tal
         } catch (NumberFormatException e) {
-            ui.displayMessage("Wrong input.. Please try again.");
+            ui.displayMessage("Vær venlig at skrive et tal imellem " + minPlayers + " og " + maxPlayers);
             registerPlayers();
+            return;
         }
 
 
-    if(totalPlayers > this.maxPlayers || totalPlayers < 2){
-        System.out.println("Your input number was higher than the allowed " + this.maxPlayers + " players");
-        registerPlayers();
+        if (totalPlayers > this.maxPlayers || totalPlayers < this.minPlayers) {
+            System.out.println("Vær venlig at skrive et tal imellem" + minPlayers + " og " + maxPlayers);
+            registerPlayers();
+        }
+
+        while (this.players.size() < totalPlayers) {
+            String playerName = ui.promptText("Tast spillernavn");
+            this.createPlayer(playerName, 0);
+        }
+
+        Collections.shuffle(this.players);
     }
 
-     while(this.players.size() < totalPlayers) {
-        String playerName = ui.promptText("Tast spiller navn");
-        this.createPlayer(playerName, 0);
-     }
 
-     Collections.shuffle(this.players);
-    }
-
-
-    private void createPlayer(String name, int score){
+    private void createPlayer(String name, int score) {
         Player p = new Player(name, score);
         players.add(p);
     }
-    public void displayPlayers(){
-        for(Player p:players){
+
+    public void displayPlayers() {
+        for (Player p : players) {
             System.out.println(p);
         }
 
@@ -77,22 +81,42 @@ public class Game {
     public void endSession() {
         ArrayList<String> playerData = new ArrayList<>();
 
-     //serialiserer player objekterner
-        for(Player p: players){
+        //serialiserer player objekterner
+        for (Player p : players) {
 
-          String s = p.toString();
-          playerData.add(s);
+            String s = p.toString();
+            playerData.add(s);
 
-      }
+        }
         //Test om promptChoice virker
         //ui.displayList(ui.promptChoice(playerData, 3, "vælg en spiller"), "Din spiller liste");
         io.saveData(playerData, "data/playerData.csv", "Name, Score");
+        ui.displayMessage("Spillet afsluttes");
     }
 
 
-    public void runGameLoop(){
-        currentPlayer=players.get(0);
-        ui.displayMessage("It's this players turn to pick: "+currentPlayer.getName());
+    public void runGameLoop() {
+        int count = 0;
+        boolean continueGame = true;
+
+        while (continueGame) {
+            if (count == players.size()) {
+                count = 0;
+            }
+
+            currentPlayer = players.get(count);
+            throwAndMove();
+            count++;
+            continueGame = ui.promptBinary("Fortsæt? (Y/N)");
+        }
+
+    }
+
+    private void throwAndMove() {
+        ui.displayMessage("Det er " + currentPlayer.getName() + "'s tur");
+    }
+
+    private void landAndAct() {
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game g = new Game("Matador",4);
+        Game g = new Game("Matador",2, 6);
         g.startSession();
         g.runGameLoop();
-
-        // run the game loop
-
         g.endSession();
+
+
+
     }
 }
