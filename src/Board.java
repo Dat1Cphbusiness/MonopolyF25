1ec00b872cdb1870a895a89d26443b0c37e26948
Tess Gaston Løvgreen <tess@lovegreen.life>
2025-03-27 22:35:11 +0100

    commit

---
+++ src/Board.java
@@ -0,0 +1,13 @@
+public class Board {
+    Field[] fields;
+
+    private void createFields(String [] data) {
+    }
+    public Field getField(int id) {
+        return null;
+    }
+    public Board (String[] fielddata, String[] carddata) {
+        Fields = new Field[fielddata.length];
+    }
+
+}
