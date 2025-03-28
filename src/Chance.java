d9780d94f6057f65c59e730cb7d8af390e4a2d32
EmilXavierVT <emilxavierthorsen@gmail.com>
2025-03-28 10:48:28 +0100

    N E D har lavet Card, CardDeck og Chance klasserne. Her kan vi lave kort, deck og trække det næste chancekort i chancedækket.

---
+++ src/Chance.java
@@ -0,0 +1,11 @@
+import java.text.NumberFormat;
+
+public class Chance extends Field {
+
+    private CardDeck cardDeck;
+
+    public Chance(int ID, String label) {
+        super(ID, label, 0, 0);
+
+    }
+}
