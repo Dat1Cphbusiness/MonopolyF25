6edc5283e9720954fd097da69a23de9afa9a4636
ebou <ebou@ndow.dk>
2025-03-28 11:56:58 +0100

    sprint5 lavet

---
+++ src/Account.java
@@ -0,0 +1,22 @@
+public class Account {
+    private int balance;
+    public Account() {
+
+    }
+
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+    public boolean withdraw(int amount) {
+        if (this.balance => amount) {
+            this.balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public int getBalance() {
+        return this.balance;
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,14 @@
 public class Player {
     private String name;
     private int score;
-    
+    private int position;
+    private Account account;
     public Player(String name, int score){
         this.name = name;
         this.score = score;
+        account = new Account();
     }
+
 
     @Override
     public String toString(){
@@ -14,6 +17,19 @@ public class Player {
 
     public String getName(){
         return name;
+    }
+
+    public int updatePosition(int position){
+        this.position += position;
+        if (this.position => 40) {
+            receive(3000);
+            this.position -= 40;
+        }
+        return this.position;
+    }
+
+    public void receive(int amount) {
+        account.deposit(amount);
     }
 
 
