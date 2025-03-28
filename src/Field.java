9efd0bd11931d57a62edc589a22e49f2f4a99939
Obliwobbi <tobiashartzberg@gmail.com>
2025-03-28 10:59:08 +0100

    Sprint4_field_toby&jesper

---
+++ src/Field.java
@@ -0,0 +1,33 @@
+public class Field {
+
+    private String label;
+    private int ID;
+    private int cost;
+    private int income;
+    private String option;
+
+    public Field(int ID, String label, int cost, int income){
+        this.ID = ID;
+        this.label = label;
+        this.cost = cost;
+        this.income = income;
+    }
+
+    @Override
+    public String toString() {
+        return this.ID + ", "  + this.label;
+    }
+
+    public String onLand(Player p){
+       return p + " er landet på felt " + this.toString();
+    }
+
+    public String onAccept(Player p){
+        return null;
+    }
+
+    public String onReject(Player p){
+        return null;
+    }
+
+}
---
+++ src/Property.java
@@ -0,0 +1,40 @@
+public class Property extends Field{
+    private Player owner;
+    private int seriesID;
+    private boolean isMonopolized;
+
+    public Property(int ID, String label, int cost, int income, int seriesID){
+        super(ID,label,cost,income);
+        this.seriesID = seriesID;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + this.seriesID;
+    }
+
+    @Override
+    public String onLand(Player p) {
+
+        /*
+        p lands on property();
+        check if property is owned
+            -If yes, check if current player is the owner,
+                --if no, player needs to pay the current owner
+            -If no, ask if player wants to buy property
+                --if yes, check for player complete series and set monopolized = true
+         */
+
+        return super.onLand(p);
+    }
+
+    @Override
+    public String onAccept(Player p) {
+        return super.onAccept(p);
+    }
+
+    @Override
+    public String onReject(Player p) {
+        return super.onReject(p);
+    }
+}
