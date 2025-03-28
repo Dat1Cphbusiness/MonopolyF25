1d6f22d3f5cfc78686135203ca5751c32f54c7eb
EmilXavierVT <emilxavierthorsen@gmail.com>
2025-03-28 11:04:45 +0100

    N E D har ændret til static

--- src/Chance.java
+++ src/Chance.java
@@ -2,7 +2,7 @@ import java.text.NumberFormat;
 
 public class Chance extends Field {
 
-    private CardDeck cardDeck;
+    private static CardDeck cardDeck;
 
     public Chance(int ID, String label) {
         super(ID, label, 0, 0);
