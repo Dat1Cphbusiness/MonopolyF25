b5bc87fbb3f7a340b2f0f3c3b8b03060d99d831f
ebou <ebou@ndow.dk>
2025-03-28 10:13:37 +0100

    tester

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,32 @@
+import static java.lang.Integer.parseInt;
+
 public class Board {
     Field[] fields;
 
     private void createFields(String [] data) {
+
+        for (int i = 0 ; i > data.length; i++) {
+            String[] values = data[i].split(",");
+            int id = Integer.parseInt(values[1].trim());
+            String FieldType = values[2].trim();
+            String label = values[3].trim();
+            int cost = Integer.parseInt(values[4].trim());
+            int income = Integer.parseInt(values[5].trim());
+            int seriesId = Integer.parseInt(values[6].trim());
+
+
+
+        }
     }
     public Field getField(int id) {
         return null;
     }
     public Board (String[] fielddata, String[] carddata) {
         Fields = new Field[fielddata.length];
+
+        createField(fielddata);
+        Chance.cardDeck = new CardDeck(carddata)
+
     }
 
 }
