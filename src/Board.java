9bfcb8bea18162289c9eb04183ce9f93a6735fc3
AsgerLA <cph-aa645@cphbusiness.dk>
2025-03-28 10:33:51 +0100

    .

--- src/Board.java
+++ src/Board.java
@@ -1,31 +1,31 @@
 import static java.lang.Integer.parseInt;
 
 public class Board {
-    Field[] fields;
+    private Field[] fields;
 
     private void createFields(String [] data) {
 
-        for (int i = 0 ; i > data.length; i++) {
+        for (int i = 0; i < data.length; i++) {
             String[] values = data[i].split(",");
-            int id = Integer.parseInt(values[1].trim());
-            String FieldType = values[2].trim();
-            String label = values[3].trim();
-            int cost = Integer.parseInt(values[4].trim());
-            int income = Integer.parseInt(values[5].trim());
-            int seriesId = Integer.parseInt(values[6].trim());
+            int id = Integer.parseInt(values[0].trim());
+            String FieldType = values[1].trim();
+            String label = values[2].trim();
+            int cost = Integer.parseInt(values[3].trim());
+            int income = Integer.parseInt(values[4].trim());
+            //int seriesId = Integer.parseInt(values[5].trim());
             fields[i] = new Field(id,label, cost, income);
 
         }
     }
     public Field getField(int id) {
 
-        return fields[i];
+        return fields[id];
     }
     public Board (String[] fielddata, String[] carddata) {
-        Fields = new Field[fielddata.length];
+        fields = new Field[fielddata.length];
 
-        createField(fielddata);
-        Chance.cardDeck = new CardDeck(carddata)
+        createFields(fielddata);
+        Chance.cardDeck = new CardDeck(carddata);
 
     }
 
