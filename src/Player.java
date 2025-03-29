19a14d942fd894df626ec72d1f9e7313ea89992d
Freddyboi123 <Frededv@outlook.dk>
2025-03-29 12:09:33 +0100

    Created account and updated player with positioning

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,7 @@ public class Player {
     private String name;
     private int score;
     private Account playerAccount;
+    private boolean hasPassedStart;
     
     public Player(String name, int score){
         this.name = name;
@@ -9,6 +10,11 @@ public class Player {
         this.playerAccount = new Account();
     }
     int updatePostion(int value){
+        if(hasPassedStart){
+            receive(4000);
+            hasPassedStart = false;
+        }
+
         int position = value;
         return position;
 
