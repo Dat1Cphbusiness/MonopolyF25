4ac466ecf607a92979c491dece32b8125ef6a2a1
daud0095 <daudibragimov2004@gmail.com>
2025-03-31 10:35:12 +0200

    -

--- src/Board.java
+++ src/Board.java
@@ -14,13 +14,39 @@ public class Board {
         for(int i = 0; i < data.length; i++){
             String[] values = data[i].split(",");
             int fieldId = Integer.parseInt(values[0].trim());
-            String fieldName = values[1];
-            String fieldProperty = values[2];
+            String fieldType = values[1];
+            String fieldLabel = values[2];
             int cost = Integer.parseInt(values[3].trim());
             int income = Integer.parseInt(values[4].trim());
             int seriesID = Integer.parseInt(values[5].trim());
+            Field f =null;
+              switch (fieldtype) {
+                  case "Chance" : f = new Chane(ID, label, income, cost);
+                      break;
+                  case "Start" : f = new Start(ID, label, income, cost);
+                      break;
+                  case : "Visit" : f = new Visit(ID, label, income, cost);
+                      break;
+                  case "Parking" : f = new Parking(ID, label, income, cost);
+                          break;
+                  case "Prison" : f = new Prison(ID, label, income, cost);
+                      break;
+                  case "Tax" : f = new Tax(ID, label, income, cost);
+                      break;
+                  case "Plot" : f = new Plot(ID, label, income, cost, seriesID);
+                      break;
+                  case "Brewery" : f = new Brewery(ID, label, income, cost, seriesID);
+                      break;
+                  case "ShippingLine" : f = new ShippingLine(ID, label, income, cost, seriesID);
+                      break;
 
-            fields[i] = new Field(fieldId,fieldProperty,cost,income);
+
+                  default: System.out.println("No such type available");
+              }
+
+
+
+            fields[i] = f;
 
         }
     }
