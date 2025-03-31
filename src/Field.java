d5f21dbe2476edbaa8a0680bd06ab3e1ccea53d6
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-31 11:55:23 +0200

    dee

--- src/Field.java
+++ src/Field.java
@@ -1,12 +1,13 @@
 public class Field {
 
+    // Attributes
     private String label;
     private int ID;
     private int cost;
     private int income;
     private String option;
 
-
+    // ______________________________________________
 
     public Field(int ID, String label, int cost, int income){
         this.ID = ID;
@@ -15,23 +16,41 @@ public class Field {
         this.income = income;
     }
 
+    // ______________________________________________
+
     @Override
     public String toString() {
         return this.ID + ", "  + this.label;
     }
 
+    // ______________________________________________
+
     public String onLand(Player p){
        return p + " er landet på felt " + this.toString();
     }
+
+    // ______________________________________________
 
     public String onAccept(Player p){
         return null;
     }
 
+    // ______________________________________________
+
     public String onReject(Player p){
         return null;
     }
+
+    // ______________________________________________
+
     public String getOption() {
         return option;
     }
-}
+
+    // ______________________________________________
+
+    public int getCost() {
+        return this.cost;
+    }
+
+} // Field class end
--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,27 @@
+import java.util.ArrayList;
+
 public class Player {
+
+    // Attributes
     private String name;
     private int score;
     private Account playerAccount;
     private boolean hasPassedStart;
     private int position = 0;
-    
+    private ArrayList<Field> deeds;
+
+    // ______________________________________________
+
     public Player(String name, int score){
         this.name = name;
         this.score = score;
         this.playerAccount = new Account();
+        deeds = new ArrayList<>();
     }
-    int updatePostion(int value){
+
+    // ______________________________________________
+
+    public int updatePostion(int value){
         if (position+value > 40){
             receive(4000);
             position = (position + value) - 40;
@@ -22,18 +33,72 @@ public class Player {
 
     }
 
+    // ______________________________________________
 
     @Override
     public String toString(){
         return name + ", "+ score;
     }
 
+    // ______________________________________________
+
     public String getName(){
         return name;
     }
 
-    void receive (int amount){
+    // ______________________________________________
 
+    public void receive (int amount){
         playerAccount.deposit(amount);
     }
-}
+
+    // ______________________________________________
+
+    // For the bank or the "universe"
+    public boolean pay(int amount){
+        if (!(playerAccount.getBalance() >= amount)) {
+            return false;
+        }
+        playerAccount.withdraw(amount);
+        return true;
+    }
+
+    // ______________________________________________
+
+    // Overloading by changing the parameters. This allows the same method name as previously used.
+    public boolean pay(int amount, Player recipient){ // Player to player payment
+
+        if (!(playerAccount.getBalance() >= amount)) {
+            return false;
+        }
+        playerAccount.withdraw(amount);
+        recipient.receive(amount);
+        return true;
+    }
+
+    // ______________________________________________
+
+    public boolean buyProperty(Field f){
+
+        if (!pay(f.getCost())) {
+            return false;
+        }
+
+        deeds.add(f);
+        return true;
+    }
+
+    // ______________________________________________
+
+    public int getTotalWorth() {
+        int amount = 0;
+
+        // For-each loop
+        for (Field f : deeds) {
+            amount += f.getCost();
+        }
+
+        return amount + playerAccount.getBalance();
+    }
+
+} // Player class end
